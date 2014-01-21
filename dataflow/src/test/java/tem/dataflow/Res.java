b72d213aefae8cf83e6dcbb7fabf2fda146662eb
Scott Tomaszewski <scott.tomaszewski@lab49.com>
2014-01-21 10:28:23 -0500

    Organize imports

--- dataflow/src/test/java/tem/dataflow/Res.java
+++ dataflow/src/test/java/tem/dataflow/Res.java
@@ -4,13 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-
 import koka.util.io.guava.Bytes;
 
 import com.google.common.io.ByteSource;
-import com.google.common.io.ByteStreams;
 import com.google.common.io.InputSupplier;
 
 /**
